221445f1013d5adb0107e3a7515904f9263f8ac6
Sahil Pan <sahilpan993@gmail.com>
2025-12-04 19:58:44 +0545

    Update AboutHead.tsx

--- components/Home/About/AboutHead.tsx
+++ components/Home/About/AboutHead.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaInstagram, FaFacebook } from "react-icons/fa";
 
 type Props = {
   about: {
@@ -19,14 +19,42 @@ const AboutHead = ({ about }: Props) => {
         </div>
         <div className="flex flex-col gap-4">
           <p className="font-bold text-white text-justify">{about.intro}</p>
-          <p className="font-semibold text-[#696969] text-justify">{about.description}</p>
+          <p className="font-semibold text-[#696969] text-justify">
+            {about.description}
+          </p>
           <div className="flex items-center text-white gap-8">
             <button className="md:px-8 md:py-2.5 px-6 py-1.5 text-white font-semibold text-md md:text-lg transition-all duration-200 rounded-lg bg-blue-700 hover:bg-blue-900 flex items-center space-x-2">
               {about.downloadButtonText}
             </button>
-            <div className="flex gap-6">
-              <FaLinkedin className="text-2xl sm:text-3xl md:text-4xl" />
-              <FaGithub className="text-2xl sm:text-3xl md:text-4xl" />
+            <div className="flex gap-4 text-yellow-400 text-2xl">
+              <a
+                href="https://github.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </a>
+              <a
+                href="https://linkedin.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin />
+              </a>
+              <a
+                href="https://facebook.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaFacebook />
+              </a>
+              <a
+                href="https://instagram.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram />
+              </a>
             </div>
           </div>
         </div>
